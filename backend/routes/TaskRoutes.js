ed73c2103cbe27ac7844e63c199df21159e5d234
santhosh775 <s.santhosh7222@gmail.com>
2024-09-09 12:35:58 +0530

    Add separate folders for frontend and backend

---
+++ backend/routes/TaskRoutes.js
@@ -0,0 +1,18 @@
+const {Router} = require("express")
+
+const { 
+    getTasks, 
+    saveTasks, 
+    updateTask, 
+    deleteTask 
+} = require("../controllers/TaskControllers")
+
+const router = Router()
+
+router.get("/get",getTasks);
+router.post("/save",saveTasks)
+router.put("/update/:id",updateTask)
+router.put("/delete/:id",deleteTask)
+
+
+module.exports = router
